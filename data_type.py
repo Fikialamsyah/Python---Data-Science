c136d61ab881f096002b217b0a80f79ce35ebf27
Fiki Alamsyah <fiki-alamsyah@pop-os.localdomain>
2020-11-05 14:51:45 +0700

    add data_type.py

---
+++ data_type.py
@@ -0,0 +1,26 @@
+# Tipe data pada python
+
+# Boolean : menyatakan True atau False
+isKemarau = True
+
+# String : menyatakan String atau karakter
+name = 'Fiki Alamsyah'
+
+# Integer : menyatakan bilangan bulat
+intNumber = 98
+
+# Float : menyatakan bilangan decimal
+floatNumber = 24.5
+
+# Complex : menyatakan pasangan angka real dan imajiner
+complexNumber = 1 + 5j
+
+# List : tipe data yg berisi berbagai tipe data dan isinya bisa diubah
+dynamicList = ['fiki', 1, True, 2, 5]
+
+# Tuple : tipe data yg berisi berbagai tipe data dan isinya tidak dapat diubah
+numberTuple = (1, 2, 5, 6, 7)
+
+# Dictionary : tipe data yg menyimpan berbagai tipe data berupa pasangan key dan valuenya
+mahasiswa = {name: 'fiki',
+             'id': 2}
