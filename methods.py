53e1c7ddaed86da52b91718e74e18604f7927580
Fiki Alamsyah <fiki-alamsyah@pop-os.localdomain>
2020-11-05 16:17:18 +0700

    add methods.py

---
+++ methods.py
@@ -0,0 +1,8 @@
+# method : fungsi yg dimiliki suatu objek
+
+# cara menggunakannya dengan menggunakan (.)
+nama = 'fiki alamsyah'
+print(nama.upper()) # method pada string untuk mengubah huruf menjadi huruf besar.
+print(nama.count('a')) # method pada string untuk menghitung huruf 'a' pada variabel nama.
+
+
